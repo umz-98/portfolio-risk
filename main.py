acd6b18224f1acf3354bcab15565accb5732c9a1
Umair <umair_khan833@hotmail.com>
2025-12-17 22:35:49 +0000

    initial setup

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
-print("Setup complete")
+# define ETF tickers
+#VUSA.L → S&P 500 (US equities)
+#VUKG.L → UK equities (FTSE All-Share)
+#VEVE.L → Global equities ex-UK
+#VAGP.L → Global bond ETF
+#VUKP.L → UK REIT / property ETF
+# set date range
+# download price data
+# check for missing values
+import yfinance
+
