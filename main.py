6eb0b5afcc8dfad43469440d293b8254dcbf5625
Umair <umair_khan833@hotmail.com>
2025-12-18 23:54:51 +0000

    changes to etf

--- main.py
+++ main.py
@@ -7,6 +7,10 @@
 # set date range
 # download price data
 # check for missing values
-import yfinance
-VUSA.L 
-
+import yfinance as yf
+import pandas as pd
+tickers = ["VUSA.L" , "VWRL.L" , "EMIM.L" , "VAGP.L", "VUKP.L"]
+prices = yf.download(tickers,
+                     start= "2017-12-18",
+                       end= "2025-12-18",)
+print(prices.head())
