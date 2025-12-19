5e6580a0ffe4df93a372d92acaa7e6415ec30f1b
Umair <umair_khan833@hotmail.com>
2025-12-19 00:06:20 +0000

    final etf list

--- main.py
+++ main.py
@@ -2,14 +2,13 @@
 # VUSA.L  -> US equities (S&P 500)
 # VWRL.L  -> Global equities
 # EMIM.L  -> Emerging market equities
-# VAGP.L  -> Global bonds
-# VUKP.L  -> UK property / REITs
+# SMEA.L  ->  European equity
 # set date range
 # download price data
 # check for missing values
 import yfinance as yf
 import pandas as pd
-tickers = ["VUSA.L" , "VWRL.L" , "EMIM.L" , "VAGP.L", "VUKP.L"]
+tickers = ["VUSA.L" , "VWRL.L" , "EMIM.L" , "SMEA.L"]
 prices = yf.download(tickers,
                      start= "2017-12-18",
                        end= "2025-12-18",)
